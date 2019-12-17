8621e11218cfbb42c986497a63925aad29e4de9c
51shangjinxin91 <57655713+51shangjinxin91@users.noreply.github.com>
2019-12-17 21:00:06 +0800

    登录模块 - 表单布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,71 +1,162 @@
 <template>
-    <div class="login-box">
-        <!-- 表单容器 -->
-        <div class="form-box">
-            <!-- 标题 -->
-            <div class="title-box">
-                <img src="../../assets/login-logo.png" alt="">
-                <span class="title">黑马面面</span>
-                <i></i>
-                <span class="sub-title">用户登录</span>
-            </div>
-        </div>
-        <img class="login-pic" src="../../assets/login_banner_ele.png" alt="">
+  <div class="login-box">
+    <!-- 表单容器 -->
+    <div class="form-box">
+      <!-- 标题 -->
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" alt />
+        <span class="title">黑马面面</span>
+        <i></i>
+        <span class="sub-title">用户登录</span>
+      </div>
+      <!-- 饿了么表单 -->
+      <el-form class="login-form" ref="form" :model="form">
+        <!-- 手机号-->
+        <el-form-item>
+          <el-input placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="form.phone"></el-input>
+        </el-form-item>
+        <!-- 密码-->
+        <el-form-item>
+          <el-input
+            placeholder="请输入密码"
+            prefix-icon="el-icon-lock"
+            show-password
+            v-model="form.password"
+          ></el-input>
+        </el-form-item>
+        <!-- 验证码-->
+        <el-form-item>
+          <!-- 行 -->
+          <el-row>
+            <!-- 列 -->
+            <el-col :span="18">
+              <el-input placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="form.captcha"></el-input>
+            </el-col>
+            <el-col :span="6">
+              <img class="captcha" @click="changeCaptcha" src="../../assets/captcha.jpg" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <!-- 用户协议 -->
+        <el-form-item>
+          <el-checkbox v-model="form.checked">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item>
+          <el-button type="primary">登录</el-button>
+          <el-button class="register-button" type="success">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+    <img class="login-pic" src="../../assets/login_banner_ele.png" alt />
+  </div>
 </template>
-
 <script>
 export default {
-    name: "login"
+  name: "login",
+  data() {
+    // 表单的数据
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+        checked: false
+      }
+    };
+  },
+  methods:{
+    changeCaptcha(){
+
+    }
+  }
 };
 </script>
 
 <style lang="less">
 /* lang="less" 多层嵌套,变量计算 */
 .login-box {
-    /* 开启弹性布局 */
-    display: flex;
-    /* 上下居中 */
-    align-items: center;
-    // 左右均分
-    justify-content: space-around;
-    // 背景线性渐变
-    background: linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
-    height: 100%;
-    .form-box {
-        width: 478px;
-        height: 550px;
-        background: #f5f5f5;
-        padding: 44px;
-        box-sizing: border-box;
-        // 标题
-        .title-box {
-            display: flex;
-            align-items: center;
-            img {
-                // emmet语法
-                // w22+h17+mr16
-                width: 22px;
-                height: 17px;
-                width: 16px;
-            }
-            .title {
-                font-size: 24px;
-                margin-right: 14px;
-            }
-            i {
-                width: 1px;
-                height: 28px;
-                margin-right: 12px;
-                background-color: #c7c7c7;
-            }
-            .sub-title {
-                font-size: 22px;
-            }
+  /* 开启弹性布局 */
+  display: flex;
+  /* 上下居中 */
+  align-items: center;
+  // 左右均分
+  justify-content: space-around;
+  // 背景线性渐变
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  height: 100%;
+  .form-box {
+    width: 478px;
+    height: 550px;
+    background: #f5f5f5;
+    padding: 44px;
+    box-sizing: border-box;
+    // 标题
+    .title-box {
+      display: flex;
+      align-items: center;
+      img {
+        // emmet语法
+        // w22+h17+mr16
+        width: 22px;
+        height: 17px;
+        width: 16px;
+      }
+      .title {
+        font-size: 24px;
+        margin-right: 14px;
+      }
+      i {
+        width: 1px;
+        height: 28px;
+        margin-right: 12px;
+        background-color: #c7c7c7;
+      }
+      .sub-title {
+        font-size: 22px;
+      }
+    }
+    // 表单盒子
+    .login-form {
+      margin-top: 27px;
+      // 验证码
+      .captcha {
+        height: 40px;
+        width: 100%;
+      }
+      // checkbox的样式
+      .el-checkbox {
+        display: flex;
+        align-items: center;
+        .el-checkbox__label {
+          display: flex;
+          align-items: center;
         }
+      }
+      // 按钮的样式
+      .el-button {
+        width: 100%;
+        margin: 0;
+      }
+      .register-button {
+        margin-top: 26px;
+      }
     }
-    .login-pic {
-
-    }
+  }
+  .login-pic {
+  }
 }
 </style>
